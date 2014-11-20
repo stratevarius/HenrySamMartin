3798e85d8c27608020c8144a68a4cb711e7fd283
Henry Szeto <ucla@hotmail.com>
2014-11-20 14:57:09 -0800

    8 organization model tests and 8 executive model tests passed

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,2 +1,12 @@
 class Organization < ActiveRecord::Base
+
+	validates_uniqueness_of :name
+	validates_presence_of :sector_type
+	validates_presence_of :web_url
+	validates_presence_of :hq_email
+	validates_presence_of :hq_address_city
+	validates_presence_of :hq_address_state
+	validates_presence_of :hq_address_zip
+	validates_presence_of :hq_telephone
+
 end
