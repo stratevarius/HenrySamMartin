b1824f7dcfb1015195dea67ec583abf6f5b5f9e3
hmhaley <mhaley@vmgstrategies.com>
2014-11-21 13:32:32 -0800

    completed flushing out the relationships controller and added an id key field to the relationships table through a migration

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -24,55 +24,28 @@ class RelationshipsController < ApplicationController
     render nothing: true, status: 204
   end
 
-  private
+private
 
-  def rel_params
-    params.require(:relationship).permit(
-		:name, 
-		:address_street, 
-		:created_at, 
-		:updated_at, 
-		:crunchbase_uuid, 
-		:Org_Types_id, 
-		:sector_type, 
-		:strat_org_constituent_type, 
-		:description, 
-		:web_url, 
-		:parent_or_child_org, 
-		:parent_org_id, 
-		:size_revenue, 
-		:size_employees_structured, 
-		:vision, 
-		:mission, 
-		:goals, 
-		:values, 
-		:motto_or_tagline, 
-		:hq_address_city, 
-		:hq_address_state, 
-		:hq_address_zip, 
-		:hq_telephone, 
-		:hq_email, 
-		:region_headquartered_in, 
-		:date_org_founded, 
-		:social_twitter, 
-		:social_linkedin, 
-		:social_facebook, 
-		:social_googleplus, 
-		:social_youtube, 
-		:social_platform_dominant, 
-		:org_special_designations, 
-		:logo_file_name, 
-		:logo_file_type, 
-		:is_verified_by_staff, 
-		:is_verified_by_thirdparty, 
-		:exec_comments_on_org, 
-		:staff_comments_on_org, 
-		:created_by_exec_id, 
-		:certifications, 
-		:source_original, 
-		:is_active, 
-		executive_ids: []
-    )
-  end
+	def rel_params
+    	params.require(:relationship).permit(
+			:executive_id, 
+			:organization_id, 
+			:relationship_type, 
+			:job_title, 
+			:is_relationship_current, 
+			:is_relationship_primary, 
+			:start_date_month, 
+			:start_date_year, 
+			:end_date_month, 
+			:end_date_year, 
+			:is_verified_by_org, 
+			:is_verified_by_staff, 
+			:is_verified_by_thirdparty, 
+			:created_by_exec_id, 
+			:exec_comments_on_relationship, 
+			:staff_comments_on_relationship, 
+			:is_active
+		)
+	end
 
 end
